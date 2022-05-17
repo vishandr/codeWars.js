149f01331ae889bca3549f5efd03b85f44b52e15
vishandr <101496759+vishandr@users.noreply.github.com>
2022-05-17 15:58:29 +0300

    Create Credit Card Mask.js

---
+++ 7 kyu/Credit Card Mask.js
@@ -0,0 +1,32 @@
+/* Find the odd int
+
+https://www.codewars.com/kata/5412509bd436bd33920011bc/train/javascript
+
+Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. 
+However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+Examples
+"4556364607935616" --> "############5616"
+     "64607935616" -->      "#######5616"
+               "1" -->                "1"
+                "" -->                 ""
+"Skippy" --> "##ippy"
+*/
+
+function maskify(cc) {
+  if (cc.length < 4){
+  return cc;
+  };
+  
+  let masked = '';
+    for (let i = 1; i <= cc.length-4; i++){
+        masked += '#'
+};
+  
+for (let i = cc.length-4; i <= cc.length-1; i++){
+    masked += cc[i]
+}
+return masked
+}
