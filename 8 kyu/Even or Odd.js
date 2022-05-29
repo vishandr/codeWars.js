4071f9246892aa4aeb487abd8d2ecaeb8b52d2eb
vishandr <101496759+vishandr@users.noreply.github.com>
2022-05-29 18:05:07 +0300

    Create Even or Odd.js

---
+++ 8 kyu/Even or Odd.js
@@ -0,0 +1,7 @@
+/*. https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/javascript
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+function even_or_odd(number) {
+  return !(number % 2) ? "Even" : "Odd"
+}
